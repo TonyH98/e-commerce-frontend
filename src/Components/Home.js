0aa776b00c8379f7e64d8fd2f579b13c9e13bc75
TonyH98 <tonyhoang@pursuit.org>
2023-02-08 20:55:46 -0500

    Filter Category Function Added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,23 +10,66 @@ const API = process.env.REACT_APP_API_URL;
 function Home(){
 
     const [products , setProducts] = useState([])
+   const [filterProducts , setFilterProducts] = useState([])
 
     useEffect(() => {
         axios
           .get(`${API}/products`)
           .then((res) => {
             setProducts(res.data);
-           
+             setFilterProducts(res.data)
           })
           .catch((c) => console.warn("catch, c"));
       }, []);
 
 
+      const map = products.map((x) => {
+        return x.category
+      })
+      
+      const category = [...new Set(map)]
+      
+      
+      
+      function handleCategory(category){
+        setFilterProducts(category)
+
+      }
+      
+      function filterCategory(e){
+        const filter = products.filter((p) => p.category === e.target.value);
+        
+    if (e.target.value === ""){
+            handleCategory(products)
+          }
+    else{
+       handleCategory(filter)
+      }
+      }
+      
+      
+
 
 return (
     <article className="flex flex-col justify-center items-center">
       <div className="sm:flex flex-wrap justify-center items-start m-2">
-          {products.map((product) => {
+        <div>
+        <label
+            htmlFor="searchProduct"
+          >
+            Select Category:
+          </label>
+          <select onChange={filterCategory}>
+            <option value="">Select All</option>
+            {category.map((c) => {
+                return(
+                  <option value={c}>{c}</option>
+                )
+            })}
+          </select>
+
+        </div>
+          {filterProducts.map((product) => {
             return(
                 <div key={product.id}>
                     <Product product={product}/>
