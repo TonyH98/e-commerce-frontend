3ec15cc87e9ebe2ca3aa0d26896cbccb9f271ebe
TonyH98 <tonyhoang@pursuit.org>
2023-07-30 21:21:51 -0400

    Minor spacing improvements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Apps from "./App.css"
+import "./App.css"
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
