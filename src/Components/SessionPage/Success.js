d7362c57473f913c9d2db430cb0146f8dac82c9a
TonyH98 <tonyhoang@pursuit.org>
2023-08-07 17:02:53 -0400

    Success Page is now mobile responsive

--- src/Components/SessionPage/Success.js
+++ src/Components/SessionPage/Success.js
@@ -1,30 +1,34 @@
 import { Link } from "react-router-dom";
+import "./Success.css"
+
 
 function Success({user}){
 
 return(
-    <div>
-        <br></br>
+    <div className="success-container">
+      
         <h1>Thank You For Your Order!</h1>
 
-        <br></br>
+        <div className="success-message-container">
 
         <p>A Confirmation of your order has been sent to your email Address</p>
 
         <p>We'll be in touch when your order has been ship </p>
+
+        </div>
         
-        <br></br>
+    
+        <div className="session-buttons-container">
 
         <Link to={`/userDetails/${user?.id}`}>
         <button className="session-btns">Go To Your Account</button>
         </Link>
 
-        <br></br>
-        <br></br>
-
         <Link to="/">
         <button className="session-btns">Return to the Home Page</button>
         </Link>
+
+        </div>
 
     </div>
 )
