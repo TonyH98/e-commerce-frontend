060982335e8da04280f0ea3c2e0384f77dccaa02
TonyH98 <tonyhoang@pursuit.org>
2023-09-06 21:47:38 -0400

    Minor CSS Improvements

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -17,16 +17,19 @@ function Footer(){
 
         <div className="footers">
             <div className="footer-section-one">
+                <h3 className="footer_header">Contact Info</h3>
                 <p className="footer-link">Career</p>
                 <p className="footer-link">Contact Us</p>
                 <p className="footer-link">Blog</p>
             </div>
             <div className="footer-section-two">
+            <h3 className="footer_header">Resources</h3>
                 <p className="footer-link">Help Center</p>
                 <p className="footer-link">Communnity Guideline</p>
                 <p className="footer-link">Return Policy</p>
             </div>
             <div className="footer-section-four">
+            <h3 className="footer_header">Policies</h3>
                 <Link to="/login">
                 <p className="footer-link">Create an Account</p>
                 </Link>
